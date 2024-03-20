38675c7a48fe608c479492378ded32e968956bee
30191400 <vandanshah612@gmail.com>
2024-03-20 16:29:29 +0530

    insert code in register.aspx.cs

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -9,9 +9,34 @@ namespace BookMyShow
 {
     public partial class Register : System.Web.UI.Page
     {
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
         protected void Page_Load(object sender, EventArgs e)
         {
+            fnConnection();
+           
+        }
 
+        protected void fnConnection()
+        {
+            try
+            {
+                string conStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+                conn = new SqlConnection(conStr);
+
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                else
+                    Response.Write("Connection Failed");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
         }
 
         protected void registerBtn_Click(object sender, EventArgs e)
@@ -22,5 +47,43 @@ namespace BookMyShow
             
             Response.Write(locationDDL.Text);
         }
+
+        protected System.Void registerBtn_Click(System.Object sender, System.EventArgs e)
+        {
+            
+            try
+            {
+                fnConnection();
+                String qry = "INSERT INTO tblTheatre(Username,Email,Password,image,Location) VALUES(@Username,@Email,@Password,@image,@Location)";
+                cmd = new SqlCommand(qry, conn);
+                cmd.Parameters.AddWithValue("Username", usernameTxt.Text);
+                cmd.Parameters.AddWithValue("Email", emailTxt.SelectedValue);
+                cmd.Parameters.AddWithValue("passwordTxt", totalSeatsTxt.Text);
+                //cmd.Parameters.AddWithValue("image", totalSeatsTxt.Text);
+                cmd.Parameters.AddWithValue("Location", locationDDL.selected);
+
+
+
+                int res = cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    successLB.Text = "Theatre is added successfully";
+                }
+                else
+                {
+                    errorLB.Text = "Insertion failed";
+                }
+                conn.Close();
+                fnBindGrid();
+            }
+            catch (Exception ex)
+            {
+                errorLB.Text = "Error: "+ ex;
+            }
+        }
+
+        
+      
+
     }
 }
